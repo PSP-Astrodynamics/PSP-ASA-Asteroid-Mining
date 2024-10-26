38e5ce44fd9e0e80466c695c77f56d8d7239ae4f
hulbertj <jwilliamhulbert@gmail.com>
2024-10-26 18:15:06 -0400

    Earth to Hektor
    Added in the earth to Hektor STK mission.  I have not done the return sequence yet

---
+++ STK_Files/Earth_to_Hektor_mission/Hektor.pl
@@ -0,0 +1,106 @@
+stk.v.12.0
+WrittenBy    STK_v12.2.0
+
+BEGIN Planet
+
+    Name		 Hektor
+
+    BEGIN PathDescription
+
+        CentralBody		 Hektor
+        UseCbEphemeris		 Yes
+
+        BEGIN EphemerisData
+
+            EphemerisSource		 None
+
+            JplIndex		 -1
+
+            JplSpiceId		 701
+
+            ApplyTDTtoTDBCorrectionForDE		 Yes
+
+        END EphemerisData
+
+    END PathDescription
+
+    BEGIN PhysicalData
+
+        GM		  8.6489438210663452e+10
+        Radius		  5.8110000000000000e+05
+        Magnitude		  0.0000000000000000e+00
+        ReferenceDistance		  0.0000000000000000e+00
+
+    END PhysicalData
+
+    BEGIN AutoRename
+
+        AutoRename		 Yes
+
+    END AutoRename
+
+    BEGIN Extensions
+
+        BEGIN ExternData
+        END ExternData
+
+        BEGIN ADFFileData
+        END ADFFileData
+
+        BEGIN AccessConstraints
+            LineOfSight IncludeIntervals
+
+            UsePreferredMaxStep No
+            PreferredMaxStep 360
+        END AccessConstraints
+
+        BEGIN Desc
+            BEGIN ShortText
+
+            END ShortText
+            BEGIN LongText
+
+            END LongText
+        END Desc
+
+        BEGIN Crdn
+        END Crdn
+
+        BEGIN Graphics
+
+            BEGIN Attributes
+
+                MarkerColor		 #ff00ff
+                LabelColor		 #ff00ff
+                LineColor		 #ff00ff
+                LineStyle		 0
+                LineWidth		 1
+                MarkerStyle		 2
+                FontStyle		 0
+
+            END Attributes
+
+            BEGIN Graphics
+
+                Show		 On
+                Inherit		 On
+                ShowLabel		 On
+                ShowPlanetPoint		 On
+                ShowSubPlanetPoint		 Off
+                ShowSubPlanetLabel		 Off
+                ShowOrbit		 On
+                NumOrbitPoints		 360
+                OrbitTime		  0.0000000000000000e+00
+                OrbitDisplay		                OneOrbit		
+                TransformTrajectory		 On
+
+            END Graphics
+        END Graphics
+
+        BEGIN VO
+        END VO
+
+    END Extensions
+
+END Planet
+
