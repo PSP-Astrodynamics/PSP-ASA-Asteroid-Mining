ba37d06282b298c384bd73927a9c4ac98b14ed40
krodb77w <krodb77w@gmail.com>
2026-03-28 15:47:13 -0400

    lamber_solver updated: delta_v will be separate in orbit solver

--- Matlab_Files/Lamert Solver/Part 2/orbit_solver.m
+++ Matlab_Files/Lamert Solver/Part 2/orbit_solver.m
@@ -2,7 +2,9 @@ function [orbit_matrix, cost]= orbit_solver(r1_vec,r2_vec)
 
     [char_star] = load_charecteristic_values();
 
-    [delta_v,orbit_matrix]=lambert_solver(r1_vec,r2_vec);
+    [orbit_matrix]=lambert_solver(r1_vec,r2_vec);
+
+    %write delta v fn separately
    
     orbital_period = sqrt((orbit_matrix(:,1).^3)./((char_star.mu)./(4*pi^2)));
 
